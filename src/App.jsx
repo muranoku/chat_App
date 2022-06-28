424c34594a7d7a38a13b48d30a4eff6e900dbdd7
garagara <ayuyeaun@gmail.com>
2022-06-28 09:31:47 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from React
+
+const App = ()  =>  {
+    return(
+        <div>
+            <h1>Medical Page chat App</h1>
+        </div>
+    );
+
+}
+
+export default App
+
+
+
+
+
